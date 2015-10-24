ef3f78c33d0be8202745bee78290d83e4f63fbd9
WeRockStar <vr_freestyle@outlook.com>
2015-10-24 23:13:49 +0700

    Check internet connection

--- app/src/main/java/com/cskku/werockstar/retrofitdemo/MainActivity.java
+++ app/src/main/java/com/cskku/werockstar/retrofitdemo/MainActivity.java
@@ -1,6 +1,11 @@
 package com.cskku.werockstar.retrofitdemo;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -10,6 +15,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.cskku.werockstar.retrofitdemo.adapter.GithubAdapter;
 import com.cskku.werockstar.retrofitdemo.api.GithubAPI;
@@ -31,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
     EditText edtUser;
     RecyclerView recyclerUserList;
     ProgressBar progressBar;
+    TextView txtError;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,8 +48,10 @@ public class MainActivity extends AppCompatActivity {
         edtUser = (EditText) findViewById(R.id.editUser);
         recyclerUserList = (RecyclerView) findViewById(R.id.gitList);
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        txtError = (TextView) findViewById(R.id.txtError);
 
         progressBar.setVisibility(View.INVISIBLE);
+        txtError.setVisibility(View.INVISIBLE);
 
         recyclerUserList.setHasFixedSize(true);
         recyclerUserList.setLayoutManager(new LinearLayoutManager(MainActivity.this));
@@ -50,38 +59,57 @@ public class MainActivity extends AppCompatActivity {
         btnSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                progressBar.setVisibility(View.VISIBLE);
 
-                String user = edtUser.getText().toString();
+                if (isNetworkConnected()) {
+                    progressBar.setVisibility(View.VISIBLE);
+                    String user = edtUser.getText().toString();
 
-                RestAdapter adapter = new RestAdapter.Builder()
-                        .setEndpoint(BASE_URL)
-                        .build();
+                    RestAdapter adapter = new RestAdapter.Builder()
+                            .setEndpoint(BASE_URL)
+                            .build();
 
-                GithubAPI git = adapter.create(GithubAPI.class);
+                    GithubAPI git = adapter.create(GithubAPI.class);
 
-                git.getFeed(user, new Callback<Github>() {
-                    @Override
-                    public void success(Github githubs, Response response) {
-                        progressBar.setVisibility(View.INVISIBLE);
+                    git.getFeed(user, new Callback<Github>() {
+                        @Override
+                        public void success(Github githubs, Response response) {
+                            progressBar.setVisibility(View.INVISIBLE);
+                            txtError.setVisibility(View.INVISIBLE);
 
-                        List<Github> list = new ArrayList<Github>();
-                        list.add(githubs);
+                            List<Github> list = new ArrayList<Github>();
+                            list.add(githubs);
 
-                        GithubAdapter githubAdapter = new GithubAdapter(list);
-                        recyclerUserList.setAdapter(githubAdapter);
+                            GithubAdapter githubAdapter = new GithubAdapter(list);
+                            recyclerUserList.setAdapter(githubAdapter);
+                            edtUser.setText("");
+                        }
 
-                        edtUser.setText("");
-                    }
+                        @Override
+                        public void failure(RetrofitError error) {
 
-                    @Override
-                    public void failure(RetrofitError error) {
-                        progressBar.setVisibility(View.INVISIBLE);
-                        Log.d("FAILURE", error.getMessage());
-                    }
-                });
+                            progressBar.setVisibility(View.INVISIBLE);
+                            txtError.setVisibility(View.VISIBLE);
+                            Log.d("FAILURE", error.getMessage());
+                        }
+                    });
+                } else {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this)
+                            .setMessage("Please connect internet")
+                            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                }
+                            });
+                    builder.show();
+                }
             }
         });
+    }
+
+    private boolean isNetworkConnected() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        return cm.getActiveNetworkInfo() != null;
     }
 
     @Override
