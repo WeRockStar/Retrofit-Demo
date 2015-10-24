882ba367cb2ed052d3b94daa482ff2475f988d48
WeRockStar <vr_freestyle@outlook.com>
2015-10-24 21:15:05 +0700

    Working now

--- app/src/main/java/com/cskku/werockstar/retrofitdemo/MainActivity.java
+++ app/src/main/java/com/cskku/werockstar/retrofitdemo/MainActivity.java
@@ -2,12 +2,66 @@ package com.cskku.werockstar.retrofitdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.cskku.werockstar.retrofitdemo.api.GithubAPI;
+import com.cskku.werockstar.retrofitdemo.model.Github;
+
+import retrofit.Callback;
+import retrofit.RestAdapter;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String BASE_URL = "https://api.github.com";
+    Button click;
+    TextView tv;
+    EditText edit_user;
+    ProgressBar pbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        click = (Button) findViewById(R.id.button);
+        tv = (TextView) findViewById(R.id.tv);
+        edit_user = (EditText) findViewById(R.id.edit);
+        pbar = (ProgressBar) findViewById(R.id.pb);
+        pbar.setVisibility(View.INVISIBLE);
+
+
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = edit_user.getText().toString();
+                pbar.setVisibility(View.VISIBLE);
+                RestAdapter adapter = new RestAdapter.Builder()
+                        .setEndpoint(BASE_URL)
+                        .build();
+
+                GithubAPI git = adapter.create(GithubAPI.class);
+
+                git.getFeed(user, new Callback<Github>() {
+                    @Override
+                    public void success(Github github, Response response) {
+                        tv.setText("Github name : "+github.getName() + "\nWebsits : "+github.getBlog() + "");
+                        pbar.setVisibility(View.INVISIBLE);
+                    }
+
+                    @Override
+                    public void failure(RetrofitError error) {
+                        pbar.setVisibility(View.INVISIBLE);
+                        Log.d("FAILURE" , error.getMessage());
+                    }
+                });
+            }
+        });
     }
 }
--- app/src/main/java/com/cskku/werockstar/retrofitdemo/api/GithubAPI.java
+++ app/src/main/java/com/cskku/werockstar/retrofitdemo/api/GithubAPI.java
@@ -1,7 +1,16 @@
 package com.cskku.werockstar.retrofitdemo.api;
 
+import com.cskku.werockstar.retrofitdemo.model.Github;
+
+import retrofit.Callback;
+import retrofit.http.GET;
+import retrofit.http.Part;
+import retrofit.http.Path;
+
 /**
  * Created by Kotchaphan on 24/10/2558.
  */
-public class GithubAPI {
+public interface GithubAPI {
+    @GET("/users/{user}")
+    public void getFeed(@Path("user") String user, Callback<Github> response);
 }
